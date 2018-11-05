167389fe4d965b98c8ee3dc37c1788538dbe76ac
jsiwek <jsiwek@pcd2n.labs.wmi.amu.edu.pl>
2018-11-05 14:43:53 +0100

    initial

---
+++ Classes5CBE/src/main/java/pl/edu/amu/dino/classes5cbe/Multiply.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.dino.classes5cbe;
+
+/**
+ *
+ * @author jsiwek
+ */
+public class Multiply {
+   public static String multiply(String a, String b){
+   return "1";} 
+}
---
+++ Classes5CBE/src/test/java/pl/edu/amu/dino/classes5cbe/MultiplyTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.dino.classes5cbe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jsiwek
+ */
+public class MultiplyTest {
+    
+    public MultiplyTest() {
+    }
+
+    @Test
+    public void testMultiply() {
+        System.out.println("multiply");
+        String a = "2";
+        String b = "3";
+        String expResult = "1";
+        String result = Multiply.multiply(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
