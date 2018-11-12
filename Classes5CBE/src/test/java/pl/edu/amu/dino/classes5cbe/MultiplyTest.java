a977f81d9630ae41e4b09e1212b693ac21d1745f
s442611 <s442611@term2.labs.wmi.amu.edu.pl>
2018-11-12 22:57:07 +0100

    new commit

--- Classes5CBE/src/test/java/pl/edu/amu/dino/classes5cbe/MultiplyTest.java
+++ Classes5CBE/src/test/java/pl/edu/amu/dino/classes5cbe/MultiplyTest.java
@@ -50,7 +50,7 @@ public class MultiplyTest {
     }
 
     @Test
-    public void testMultiply1() {
+    public void testMultiply19() {
         System.out.println("multiply");
         String a = "2";
         String b = "3";
@@ -60,7 +60,7 @@ public class MultiplyTest {
     }
 
     @Test
-    public void testMultiply2() {
+    public void testMultiply21() {
         System.out.println("multiply");
         String a = "2";
         String b = "3";
@@ -109,4 +109,30 @@ public class MultiplyTest {
         int result = Multiply.result1;
         assertNotEquals(expResult, result);
     }
-}
+    @Test
+    public void testMultiply12()
+    { String a="2";
+    String b="4";
+    String expr="8";
+    String r=Multiply.multiply(a, b);
+    assertEquals(expr,r);
+    }
+    @Test
+    public void testMultiply13()
+    { String a="hello";
+    String b="kolo";
+    String expr="2";
+    String r=Multiply.multiply(a, b);
+    assertNotEquals(expr,r);
+    }
+    @Test
+    public void testMultiply14()
+    { String a="student";
+    String b="kielo";
+    String expr="1";
+    String r=Multiply.multiply(a, b);
+    assertNotEquals(expr,r);
+    }
+    
+   
+  }
